d55a456371372eb91c2a613e1bf1f9d42803dcf4
YiPort <yejinbin1024@163.com>
2023-04-17 01:59:16 +0800

    feat: 新增API网关服务模块，选用Spring Cloud Gateway作为API网关,实现了路由转发、访问控制、流量染色

---
+++ ypapi-gateway/src/main/java/cn/yiport/ypapigateway/YpapiGatewayApplication.java
@@ -0,0 +1,53 @@
+package cn.yiport.ypapigateway;
+
+import cn.yiport.project.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+@EnableDubbo
+@Service
+public class YpapiGatewayApplication {
+
+    @DubboReference
+    private DemoService demoService;
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(YpapiGatewayApplication.class, args);
+        YpapiGatewayApplication application = context.getBean(YpapiGatewayApplication.class);
+        String result = application.doSayHello("world");
+        String result2 = application.doSayHello2("world");
+        System.out.println("result: " + result);
+        System.out.println("result: " + result2);
+    }
+
+    public String doSayHello(String name) {
+        return demoService.sayHello(name);
+    }
+
+    public String doSayHello2(String name) {
+        return demoService.sayHello2(name);
+    }
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("tobaidu", r -> r.path("/baidu")
+//                        .uri("https://www.baidu.com"))
+//                .route("toyiportcn", r -> r.path("/yiportcn")
+//                        .uri("http://yiport.cn"))
+//                .build();
+//    }
+
+}
