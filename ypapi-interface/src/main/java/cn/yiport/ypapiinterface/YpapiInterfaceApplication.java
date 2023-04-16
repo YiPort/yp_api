ef24401152e6c407ca41d1306ec58512e82f1a2c
YiPort <yejinbin1024@163.com>
2023-04-17 02:04:06 +0800

    feat: 新增模拟API接口模块

---
+++ ypapi-interface/src/main/java/cn/yiport/ypapiinterface/YpapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package cn.yiport.ypapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YpapiInterfaceApplication.class, args);
+    }
+
+}
